b508a80292345d13fc5432b0e5dcf5a5658bf924
Abdillah Sulthan Naufal Panggabean <abdillahsulthan12@gmail.com>
2024-06-08 18:18:28 +0700

    bug: protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { auth } from "./services/FirebaseConfig";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 
 const App = () => {
-  const [user, setUser] = React.useState<any>(null);
-
-  React.useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setUser(user);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
-        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
